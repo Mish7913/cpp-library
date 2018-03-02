6ed77b8c0cb1e5d1da315c521e058e6c8ee9c640
Misha <mish7913@gmail.com>
2018-03-02 18:31:36 +0200

    Update str.h

--- str.h
+++ str.h
@@ -26,6 +26,21 @@
 #include <string.h>
 
 namespace str {
+	std::wstring str_to_wstr(const std::string& s);
+	std::string wstr_to_str(const std::wstring& ws);
+	std::string substr(std::string str, int pos, int len);
+	
+	std::string substr(std::string str, int pos, int len){
+		std::map < int, std::string >  get_char;  int  num_char = 0;   std::string   str_char,   sTmp;
+		std::wstring    wstr_char    =   str_to_wstr   (  str  );    std::wstring    wstr_get,    tmp;
+		for ( int i = 0;  i < (int) wstr_char.size();  i++ ) {  if ( (int) wstr_char[num_char] < 0 ) {
+		tmp  =  wstr_char.substr( num_char,  2 );  sTmp  =  wstr_to_str( tmp );   str_char   =   sTmp;
+		get_char[i] = str_char; num_char = num_char + 2; } else { tmp = wstr_char.substr(num_char, 1);
+		sTmp = wstr_to_str(tmp);  str_char = sTmp;  get_char[i] = str_char; num_char = num_char + 1; }
+		if ( num_char  ==  (int)  wstr_char.size() ) { break; }  }   str_char.clear();   sTmp.clear();
+		for ( int i = 0;  i < len;  i++ ) {str_char = str_char + get_char[pos + i];} return  str_char;
+	};
+	
 	std::wstring str_to_wstr(const std::string& s) {
 		std::string curLocale = setlocale(LC_ALL, ""); const char* ch_source = s.c_str();
 		size_t     ch_dsize    =    mbstowcs    (NULL,     ch_source,     0)     +     1;
@@ -41,4 +56,5 @@ namespace str {
 		wcstombs (ch_dest, (wchar_t*) ch_source,  ch_dsize);  std::string result  =  ch_dest;
 		delete [] ch_dest;   setlocale(  LC_ALL,   curLocale.c_str()  );    return    result;
 	}
+	
 }
